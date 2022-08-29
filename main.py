a00f06d197df1bc78201fd0229675c9148e7e15b
Johan Hay <johanhay98@gmail.com>
2022-08-29 07:04:04 +0200

    v1

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hi there'
+    return render_template('body.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
